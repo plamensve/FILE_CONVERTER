782b02497552da179a6978cb31f7337f4c848eb3
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-07-28 00:01:53 +0300

    Update FileConverter.py

--- FileConverter.py
+++ FileConverter.py
@@ -1,10 +1,18 @@
+import os
+import sys
 import tkinter as tk
 from ui.main_window import MainWindow
 
 root = tk.Tk()
 root.title("File Converter")
 root.geometry('900x800')
-root.iconbitmap("data/logo.ico")
+
+if getattr(sys, 'frozen', False):
+    icon_path = os.path.join(sys._MEIPASS, 'data', 'logo.ico')
+else:
+    icon_path = os.path.join('data', 'logo.ico')
+
+root.iconbitmap(icon_path)
 
 app = MainWindow(root)
 root.mainloop()
